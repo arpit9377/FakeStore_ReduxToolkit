cad72ee4877e83981a03bad5b1367cd3af2b018d
Arpit Kumar Yadav <72245696+arpit9377@users.noreply.github.com>
2023-11-17 12:48:37 +0530

    Update Products.jsx

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,35 +1,35 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { fetchProducts, selectProducts, selectProduct, setProducts } from '../Redux/ProductSlice';
-import './Products.css'; 
+import { fetchProducts, selectProducts, fetchProductById } from '../Redux/ProductSlice'; 
+import './Products.css';
 
 const Products = () => {
   const dispatch = useDispatch();
   const products = useSelector(selectProducts);
 
   useEffect(() => {
-    dispatch(fetchProducts())
-      .then((response) => {
-        dispatch(setProducts(response.payload));
-      })
+    dispatch(fetchProducts());
   }, [dispatch]);
 
-  const handleProductClick = (product) => {
-    dispatch(selectProduct(product));
+  const handleProductClick = async (product) => {
+    await dispatch(fetchProductById(product.id));
   };
 
   return (
-    <div className="product-container">
-      {products.map((product) => (
-        <div className="product-card" key={product.id}>
-          <Link to={`/product/${product.id}`} onClick={() => handleProductClick(product)}>
-            <h3>{product.title}</h3>
-            <img src={product.image} alt={product.title} className="product-image" />
-          </Link>
-        </div>
-      ))}
-    </div>
+    <>
+      <div className='heading'>Product List</div>
+      <div className="product-container">
+        {products.map((product) => (
+          <div className="product-card" key={product.id}>
+            <Link to={`/product/${product.id}`} onClick={() => handleProductClick(product)}>
+              <h3>{product.title}</h3>
+              <img src={product.image} alt={product.title} className="product-image" />
+            </Link>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
