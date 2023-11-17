74e74faf9a0f58678e2eaa427ac5b5bd064c2dbb
Arpit Kumar Yadav <72245696+arpit9377@users.noreply.github.com>
2023-11-17 16:51:19 +0530

    Update Product.jsx
    Resolved Refresh Issue

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { selectSelectedProduct } from '../Redux/ProductSlice';
-import './Product.css'; 
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom'; 
+import { fetchProductById, selectSelectedProduct } from '../Redux/ProductSlice';
+import './Product.css';
 
 const ProductDetail = () => {
+  const { id } = useParams(); // Get the id from route
+  const dispatch = useDispatch();
   const selectedProduct = useSelector(selectSelectedProduct);
 
+  useEffect(() => {
+    // Dispatch the action to fetch product data based on the id parameter
+    if (id) {
+      dispatch(fetchProductById(id));
+    }
+  }, [dispatch, id]);
+
   if (!selectedProduct) {
-    return <div>Loading...</div>; 
+    return <div>Loading...</div>;
   }
 
   return (
