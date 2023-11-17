0c1a9243b2f54fa74a23fc2ca866688e79c0d6c9
Arpit Kumar Yadav <72245696+arpit9377@users.noreply.github.com>
2023-11-17 12:49:38 +0530

    Update Product.jsx

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -6,14 +6,20 @@ import './Product.css';
 const ProductDetail = () => {
   const selectedProduct = useSelector(selectSelectedProduct);
 
+  if (!selectedProduct) {
+    return <div>Loading...</div>; 
+  }
+
   return (
-    <div className="product-detail-container">
-      <h2 className="product-detail-title">{selectedProduct.title}</h2>
-      <img src={selectedProduct.image} alt={selectedProduct.title} className="product-detail-image" />
-      <p className="product-detail-description">{selectedProduct.description}</p>
-      <p className="product-detail-price">Price: ${selectedProduct.price}</p>
-    
-    </div>
+    <>
+      <div className='heading'>Product Details</div>
+      <div className="product-detail-container">
+        <h2 className="product-detail-title">{selectedProduct.title}</h2>
+        <img src={selectedProduct.image} alt={selectedProduct.title} className="product-detail-image" />
+        <p className="product-detail-description">{selectedProduct.description}</p>
+        <p className="product-detail-price">Price: ${selectedProduct.price}</p>
+      </div>
+    </>
   );
 };
 
