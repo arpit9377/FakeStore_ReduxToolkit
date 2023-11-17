d7d419a2daefe836192efc87077f722584cc9bb4
Arpit Kumar Yadav <72245696+arpit9377@users.noreply.github.com>
2023-11-17 12:48:00 +0530

    Update ProductSlice.jsx

--- src/Redux/ProductSlice.jsx
+++ src/Redux/ProductSlice.jsx
@@ -1,11 +1,5 @@
-// ProductSlice.jsx
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
-
-export const fetchProducts = createAsyncThunk('product/fetchProducts', async () => {
-  const response = await axios.get('https://fakestoreapi.com/products');
-  return response.data;
-});
 
 const productSlice = createSlice({
   name: 'product',
@@ -14,16 +8,35 @@ const productSlice = createSlice({
     selectedProduct: null,
   },
   reducers: {
-    selectProduct: (state, action) => {
-      state.selectedProduct = action.payload;
-    },
     setProducts: (state, action) => {
       state.products = action.payload;
+    },
+    selectProduct: (state, action) => {
+      state.selectedProduct = action.payload;
     },
   },
 });
 
 export const { selectProduct, setProducts } = productSlice.actions;
+
+export const fetchProducts = () => async (dispatch) => {
+  try {
+    const response = await axios.get('https://fakestoreapi.com/products');
+    dispatch(setProducts(response.data));
+  } catch (error) {
+    console.error('Error fetching products:', error);
+  }
+};
+
+export const fetchProductById = (productId) => async (dispatch) => {
+  try {
+    const response = await axios.get(`https://fakestoreapi.com/products/${productId}`);
+    dispatch(selectProduct(response.data));
+  } catch (error) {
+    console.error('Error fetching product details:', error);
+  }
+};
+
 export const selectProducts = (state) => state.product.products;
 export const selectSelectedProduct = (state) => state.product.selectedProduct;
 
